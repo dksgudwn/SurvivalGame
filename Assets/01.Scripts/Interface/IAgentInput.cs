166df8d65bbd36afb4dacb0a9f6afb3035ae67db
dksgudwn <dksgudwn620@gmail.com>
2024-06-04 09:18:39 +0900

    요잉

---
+++ Assets/01.Scripts/Interface/IAgentInput.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public interface IAgentInput
+{
+    public UnityEvent<Vector2> OnMovementKeyPress { get; set; }
+}
