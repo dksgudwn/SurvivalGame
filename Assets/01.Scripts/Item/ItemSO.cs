d816f92f4fac0ddea94985db8a6ae49782b15e3b
9mean3 <gminseong114@gmail.com>
2024-09-27 14:31:52 +0900

    Item SOs :+1:

---
+++ Assets/01.Scripts/Item/ItemSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Item")]
+public class ItemSO : ScriptableObject
+{
+    public string Name;
+    public Sprite Image;
+    public string Description;
+
+
+}
---
+++ Assets/01.Scripts/Item/WeaponSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Item/Weapon")]
+public class WeaponSO : ItemSO
+{
+    public float Damage;
+    public float Range;
+    public float AttackSpeed;
+}
